de52f8e8db9e8c5f33ddcda28e940ac6304c91e9
ngwenyatm <thabsnspaces@gmail.com>
2024-02-04 20:01:09 -0800

    .

--- main.c
+++ main.c
@@ -7,44 +7,37 @@
 * Return: 0 on success
 */
 
-globeV varG = {NULL, NULL, NULL, 0};
-
 int main(int ac, char *argv[])
 {
-    char *buffer;   
-    FILE *f;      
-    size_t bsize = 0; 
-    ssize_t charNum = 1; 
-    stack_t *temp = NULL; 
-    unsigned int count = 0; 
-    
-    if (ac != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n"); 
-        exit(EXIT_FAILURE); 
-    }
+	char *buffer;
+	FILE *f;
+	size_t bsize = 0;
+	ssize_t charNum = 1;
+	stack_t *temp = NULL;
+	unsigned int count = 0;
 
-    f = openFile(argv[1]);
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-    while (charNum != 0) 
-    {
-        buffer = NULL;
+	f = openFile(argv[1]);
 
-        charNum = getline(&buffer, &bsize, f);
-        varG.theOps = buffer; 
-        count++;
+	while (charNum != 0)
+	{
+		buffer = NULL;
+		charNum = getline(&buffer, &bsize, f);
+		varG.theOps = buffer;
+		count++;
 
-        if (charNum > 0)
-        {
-            callMethod(buffer, &temp, count, f); 
-        }
-
-        
-        free(buffer);
-    }
-
-    freeDLL(temp);
-    fclose(f);
-
-    return (0);
+		if (charNum > 0)
+		{
+			callMethod(buffer, &temp, count, f);
+		}
+		free(buffer);
+	}
+	freeDLL(temp);
+	fclose(f);
+	return (0);
 }
